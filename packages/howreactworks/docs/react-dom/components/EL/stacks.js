8ddbc17d55bfa1b5b781ea3d2288bee0d0ba2a12
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2023-10-03 12:34:43 +0100

    screenshots fixes & add some stacks & more on commit work

--- packages/howreactworks/docs/react-dom/components/EL/stacks.js
+++ packages/howreactworks/docs/react-dom/components/EL/stacks.js
@@ -82,3 +82,102 @@ export const S_02_ROOT_RENDER_2 = [
     statement: `RegisterServiceWorker();`
   },
 ];
+
+
+export const S_05_WORK_LOOP_PERFORM_CW_01 = [
+  {
+    statement: `if ((executionContext & (RenderContext | CommitContext)) !== NoContext) {
+  throw new Error('Should not already be working.');
+}`,
+  },
+  {
+    statement: `const originalCallbackNode = root.callbackNode;
+const didFlushPassiveEffects = flushPassiveEffects();
+
+if (didFlushPassiveEffects && root.callbackNode !== originalCallbackNode) {
+  return null;
+}`,
+  },
+  {
+    statement: `let lanes = getNextLanes(
+  root,
+  root === workInProgressRoot ? workInProgressRootRenderLanes : NoLanes,
+);
+if (lanes === NoLanes) {
+  return null;
+}`,
+  },
+  {
+    statement: `const shouldTimeSlice =
+  !includesBlockingLane(root, lanes) &&
+  !includesExpiredLane(root, lanes) &&
+  (disableSchedulerTimeoutInWorkLoop || !didTimeout);`,
+  },
+  {
+    statement: `let exitStatus = shouldTimeSlice
+  ? renderRootConcurrent(root, lanes)
+  : renderRootSync(root, lanes);`,
+  },
+  {
+    statement: `if (renderWasSuccessfull) {
+  root.finishedWork = finishedWork;
+  root.finishedLanes = lanes;
+  finishConcurrentRender(root, exitStatus, finishedWork, lanes);
+} else {
+  // manage errors and suspense
+}`,
+  },
+  {
+    statement: `ensureRootIsScheduled(root);
+return getContinuationForRoot(root, originalCallbackNode);`,
+  },
+]
+
+export const S_05_WORK_LOOP_RENDER_SYNC_01 = [
+  {
+    statement: `const prevExecutionContext = executionContext;
+executionContext |= RenderContext;
+const prevDispatcher = pushDispatcher(root.containerInfo);`,
+  },
+  {
+    statement: `if (workInProgressRoot !== null || workInProgressRootRenderLanes !== lanes) {
+   // [...] some work
+   // highlight-next-line
+   prepareFreshStack(root, lanes);
+}`,
+  },
+  {
+    statement: `do {
+  try {
+    if (didSuspendDuringHydration) {
+      resetWIPStack();
+      workInProgressRootExitStatus = RootDidNotComplete;
+      break;
+    }
+    
+    // [...] Other branches to break when needed
+
+    // highlight-next-line
+    workLoopSync();
+    // Why a break here you wonder ? Hint: there is no break in the catch block
+    break;
+  } catch (e) {
+    // highlight-next-line
+    handleThrow(root, e);
+  }
+} while (true)`,
+  },
+  {
+    statement: `if (didSuspendInShell) {
+  root.shellSuspendCounter++;
+}
+
+executionContext = prevExecutionContext;
+workInProgressRoot = null;
+workInProgressRootRenderLanes = NoLanes;`,
+  },
+  {
+    statement: `finishQueueingConcurrentUpdates();
+return workInProgressRootExitStatus;`,
+  },
+]
