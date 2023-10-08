1cbde3dc1cfde2b1cb9ecab8c7e49b1cb931b3f6
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2023-10-08 16:57:56 +0100

    change social card

--- packages/howreactworks/docusaurus.config.js
+++ packages/howreactworks/docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/how-react-works.png',
       navbar: {
         title: 'How react works',
         logo: {
